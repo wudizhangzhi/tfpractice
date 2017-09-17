2ca59cfc9162b63fb064ec18e3324d0aa0828834
张智超 <zhangzhichao@promote.cache-dns.local>
2017-09-17 19:53:44 +0800

    accuracy 61%

--- tutorials/imagenet/Cifar_10.py
+++ tutorials/imagenet/Cifar_10.py
@@ -4,10 +4,15 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import os
+import re
 
 
 class Cifar_10:
-    def __init__(self, n_labels, batch_size=32, maxstep=8000, epochs=10, img_h=32, img_w=32, img_d=3, lr=0.9):
+    def __init__(self, n_labels, batch_size=128, maxstep=8000, epochs=10, img_h=32, img_w=32, img_d=3, lr=0.001,
+                 lr_decay_rate=0.9, decay_per_step=1000, data_dir='cifar-10-batches-py', traindata_name='data_batch_*',
+                 testdata_name='test_batch',
+                 namespace_file='batches.meta'):
         self.IMG_H = img_h
         self.IMG_W = img_w
         self.IMG_D = img_d
@@ -15,26 +20,37 @@ class Cifar_10:
         self.n_labels = n_labels
         self.LR = lr
         self.batch_size = batch_size
-        self.epochs = epochs
+        self.epochs = epochs  # TODO delete
         self.maxstep = maxstep
+        self.lr_decay_rate = lr_decay_rate
+        self.decay_per_step = decay_per_step
+        # data filename
+        self.traindata_filelist = []
+        assert os.path.exists(data_dir)
+        filelist = os.listdir(data_dir)
+        for filename in filelist:
+            if re.match(traindata_name, filename):
+                self.traindata_filelist.append(os.path.join(data_dir, filename))
+        self.testdata_name = os.path.join(data_dir, testdata_name)
+        self.namespace_file = os.path.join(data_dir, namespace_file)
 
         self.sess = tf.Session()
         self._build_net()
+        self.saver = tf.train.Saver()
 
     def _build_net(self):
         print('=== start build graph ===')
         # placeholder
-        self.tf_images = tf.placeholder(tf.float32, shape=[None, self.IMG_H * self.IMG_W * self.IMG_D])
-        self.tf_labels = tf.placeholder(tf.int32, shape=[None, 1])
-
-        # self.tf_labels = tf.reshape(self.tf_labels, [-1])
-        self.tf_images_reshape = tf.reshape(self.tf_images, shape=[-1, self.IMG_H, self.IMG_W, self.IMG_D])
+        with tf.variable_scope('Input'):
+            self.tf_images = tf.placeholder(tf.float32, shape=[None, self.IMG_H, self.IMG_W, self.IMG_D], name='images') / 255
+            self.tf_labels = tf.placeholder(tf.int32, shape=[None, 1], name='labels')
+            tf.summary.image('images', self.tf_images)
         # wieght, bias initilizer
-        w_initilizer, b_initilizer = tf.random_normal_initializer(0.1, 0.3), tf.constant_initializer(0.1)
+        w_initilizer, b_initilizer = tf.truncated_normal_initializer(stddev=5e-2), tf.constant_initializer(0)
 
         # 第一层卷积层
         with tf.variable_scope('C_layer_1'):
-            conv1 = tf.layers.conv2d(self.tf_images_reshape,
+            conv1 = tf.layers.conv2d(self.tf_images,
                                      filters=64,
                                      kernel_size=5,
                                      strides=1,
@@ -44,7 +60,8 @@ class Cifar_10:
 
             pool1 = tf.layers.max_pooling2d(conv1,
                                             pool_size=2,
-                                            strides=2)  # shape [batch, height/2, width/2, 64]
+                                            strides=2,
+                                            padding='same')  # shape [batch, height/2, width/2, 64]
             # norm1
             norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
                               name='norm2')
@@ -62,7 +79,8 @@ class Cifar_10:
                               name='norm2')
             pool2 = tf.layers.max_pooling2d(norm2,
                                             pool_size=2,
-                                            strides=2)  # shape [batch, height/4, width/4, 128]
+                                            strides=2,
+                                            padding='same')  # shape [batch, height/4, width/4, 128]
 
         # reshape
         # h_w_d = tf.reduce_prod(shape[1:])
@@ -71,35 +89,68 @@ class Cifar_10:
         # 隐藏层1
         with tf.variable_scope('Hidden_layer_1'):
             hidden_1 = tf.layers.dense(pool2_reshape,
-                                       128*2,
+                                       128 * 2,
+                                       activation=tf.nn.relu,
+                                       kernel_initializer=w_initilizer,
+                                       bias_initializer=b_initilizer)
+
+        # 隐藏层2
+        with tf.variable_scope('Hidden_layer_2'):
+            hidden_2 = tf.layers.dense(hidden_1,
+                                       192,
                                        activation=tf.nn.relu,
                                        kernel_initializer=w_initilizer,
                                        bias_initializer=b_initilizer)
 
         # 输出层
-        with tf.variable_scope('Hidden_layer_2'):
-            output = tf.layers.dense(hidden_1, self.n_labels,
-                                     kernel_initializer=w_initilizer,
-                                     bias_initializer=b_initilizer)
+        with tf.variable_scope('Hidden_layer_3'):
+            self.output = tf.layers.dense(hidden_2, self.n_labels,
+                                          kernel_initializer=w_initilizer,
+                                          bias_initializer=b_initilizer)
 
         # accuracy
         with tf.variable_scope('Accuracy'):
             accuracy, self.accuracy_op = tf.metrics.accuracy(labels=self.tf_labels,
-                                                             predictions=tf.argmax(output, axis=1))
+                                                             predictions=tf.argmax(self.output, axis=1))
             tf.summary.scalar('accuracy', self.accuracy_op)  # add loss to scalar summary
         # loss
         with tf.variable_scope('Loss'):
             # self.losses = tf.losses.softmax_cross_entropy(logits=output, onehot_labels=self.tf_labels)
-            cross_loss = tf.losses.sparse_softmax_cross_entropy(logits=output, labels=self.tf_labels)
+            cross_loss = tf.losses.sparse_softmax_cross_entropy(logits=self.output, labels=self.tf_labels)
             self.losses = tf.reduce_mean(cross_loss)
             tf.summary.scalar('loss', self.losses)  # add loss to scalar summary
 
-        self.train_op = tf.train.AdamOptimizer(self.LR).minimize(self.losses)
+        with tf.variable_scope('Train'):
+            global_step = tf.Variable(0, trainable=False)
+            LR = tf.train.exponential_decay(self.LR,
+                                            global_step,
+                                            self.decay_per_step,
+                                            self.lr_decay_rate,
+                                            staircase=True)
+            self.train_op = tf.train.AdamOptimizer(LR).minimize(self.losses, global_step=global_step)
 
         # init
         self.init_op = tf.group(tf.global_variables_initializer(),
                                 tf.local_variables_initializer())  # the local var is for accuracy_op
         print('=== end build graph ===')
+
+    @classmethod
+    def _handler_images_data(cls, proto):
+        # return np.vstack((proto[:, :1024], proto[:, 1024:1024*2], proto[:, 1024*2:1024*3])).T.reshape((-1, 32, 32, 3))
+        return np.transpose(proto.reshape((-1, 3, 1024)), axes=[0, 2, 1]).reshape((-1, 32, 32, 3))
+
+    def produce_test_data(self, testfilename):
+        # test data
+        with open(testfilename, 'rb') as f:
+            d_data = pickle.load(f, encoding='bytes')
+            batch_labels = d_data[b'labels']
+            batch_images = d_data[b'data']
+
+            assert len(batch_labels) == len(batch_images)
+            batch_labels = np.array(batch_labels)[:, np.newaxis]
+            batch_images = self._handler_images_data(batch_images)
+            self.dataset_test = tf.contrib.data.Dataset.from_tensor_slices((batch_images, batch_labels))
+        return self.dataset_test
 
     def produce_data(self, filelist, testfilename, label_name_file=None):
         assert isinstance(filelist, list)
@@ -115,6 +166,7 @@ class Cifar_10:
                 assert len(batch_labels) == len(batch_images)
                 batch_labels = np.array(batch_labels)[:, np.newaxis]
                 labels_list.append(batch_labels)
+                batch_images = self._handler_images_data(batch_images)
                 images_list.append(batch_images)
                 # dataset = tf.contrib.data.Dataset.from_tensor_slices((batch_images, batch_labels))
                 # dataset_list.append(dataset)
@@ -125,17 +177,10 @@ class Cifar_10:
         self.data_set = tf.contrib.data.Dataset.from_tensor_slices((images, labels))
 
         # test data
-        with open(testfilename, 'rb') as f:
-            d_data = pickle.load(f, encoding='bytes')
-            batch_labels = d_data[b'labels']
-            batch_images = d_data[b'data']
-
-            assert len(batch_labels) == len(batch_images)
-            batch_labels = np.array(batch_labels)[:, np.newaxis]
-            self.dataset_test = tf.contrib.data.Dataset.from_tensor_slices((batch_images, batch_labels))
+        self.produce_test_data(testfilename)
 
         # 处理名称 label_names
-        label_names = []
+        self.label_names = []
         if label_name_file:
             if not tf.gfile.Exists(label_name_file):
                 print("can't find %s" % label_name_file)
@@ -144,17 +189,18 @@ class Cifar_10:
                     d_data = pickle.load(f, encoding='bytes')
                     label_names = d_data[b'label_names']
 
-        return self.data_set, self.dataset_test, label_names
+        return self.data_set, self.dataset_test, self.label_names
 
     def train(self, is_save=False):
         print('=== start retrieve data ===')
-        train_file_list = ['cifar-10-batches-py/data_batch_1', 'cifar-10-batches-py/data_batch_2']
-        train_dataset, test_dataset, label_names = self.produce_data(train_file_list, 'cifar-10-batches-py/test_batch',
-                                                                     'cifar-10-batches-py/batches.meta')
+        # train_file_list = ['cifar-10-batches-py/data_batch_1', 'cifar-10-batches-py/data_batch_2']
+        train_dataset, test_dataset, label_names = self.produce_data(self.traindata_filelist, self.testdata_name,
+                                                                     self.namespace_file)
 
         train_dataset = train_dataset.batch(self.batch_size)
         train_dataset = train_dataset.shuffle(buffer_size=10000)
-        train_dataset = train_dataset.repeat(self.epochs)
+        # train_dataset = train_dataset.repeat(self.epochs)
+        train_dataset = train_dataset.repeat()
         iterator = train_dataset.make_initializable_iterator()
         next_element = iterator.get_next()
 
@@ -174,13 +220,13 @@ class Cifar_10:
         print('=== start training ===')
         for step in range(self.maxstep):
             batch_images, batch_labels = self.sess.run(next_element)
-            self.sess.run(self.train_op, feed_dict={
+            _, _loss = self.sess.run([self.train_op, self.losses], feed_dict={
                 self.tf_labels: batch_labels,
                 self.tf_images: batch_images,
             })
             if step % 50 == 0:
                 batch_image_test, batch_labels_test = self.sess.run(next_element_test)
-                _loss, _accuracy, record = self.sess.run([self.losses, self.accuracy_op, merged], feed_dict={
+                _accuracy, record = self.sess.run([self.accuracy_op, merged], feed_dict={
                     self.tf_labels: batch_labels_test,
                     self.tf_images: batch_image_test,
                 })
@@ -190,10 +236,29 @@ class Cifar_10:
         print('=== end training ===')
         print('=== save ===')
         if is_save:
-            saver = tf.train.Saver()  # define a saver
-            saver.save(self.sess, 'params', write_meta_graph=False)  # meta_graph not recommended
+            self.saver.save(self.sess, 'params', write_meta_graph=False)  # meta_graph not recommended
+
+    def predict(self, images):
+        self.saver.restore(self.sess, 'params')
+        predicts = self.sess.run(self.output, feed_dict={self.tf_images: images})
+
+        return [self.label_names[predict] for predict in predicts]
+
+    @classmethod
+    def plot_predicts(cls, images, predicts):
+        num = len(images)
+        assert len(predicts) == num
+        img_per_row = num // 2
+        fig, ax = plt.subplots(2, img_per_row)
+        for i in range(img_per_row * 2):
+            ax[i // img_per_row, i % img_per_row].imshow(images[i])
+            ax[i // img_per_row, i % img_per_row].title(predicts[i])
+        plt.show()
+
+    def test_predict(self):
+        self.produce_test_data('cifar-10-batches-py/test_batch')
 
 
 if __name__ == '__main__':
-    c = Cifar_10(n_labels=10)
-    c.train()
+    c = Cifar_10(n_labels=10, maxstep=100000, decay_per_step=5000, lr=0.01)
+    c.train(is_save=True)
